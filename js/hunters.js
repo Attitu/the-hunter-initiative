7b66081478405f93bc5899c862dff16f68987d07
Attitu <hellen.atitu@gmail.com>
2021-05-17 22:56:31 +0300

    done with small devices

--- js/hunters.js
+++ js/hunters.js
@@ -10,12 +10,15 @@ arrow.addEventListener('click', function(){
 })
 
 let bars = document.querySelector(".fa-bars");
+// let items = document.querySelector(".items")
 bars.addEventListener('click',function(){
 	let items = document.querySelector('.items')
 	if (items.style.display === "none") {
-		items.style.display = "block"
+		items.style.display = "block";
+		items.style.backgroundColor = "aqua";
+		items.style.width = "100%"
 	}
 	else{
-		items.style.display = "block"
+		items.style.display = "none";
 	}
 })
